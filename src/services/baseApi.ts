585620e79250076a5fecfd461afdab50b745db3c
JoshuaMusyoka <joshuamusyokar@gmail.com>
2025-10-27 21:22:08 +0300

    https update

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -10,7 +10,8 @@ import type {
 
 const baseQuery = fetchBaseQuery({
   baseUrl:
-    import.meta.env.VITE_API_BASE_URL || "http://api.bengalproperty.com/api/v1",
+    import.meta.env.VITE_API_BASE_URL ||
+    "https://api.bengalproperty.com/api/v1",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
